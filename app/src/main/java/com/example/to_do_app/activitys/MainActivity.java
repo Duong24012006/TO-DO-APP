9084eb00b770e29add4f26a4ba972c55054cc563
Duong24012006 <duongnguyen.24012006@gmail.com>
2025-10-28 22:34:29 +0700

    [Task-02] update layout theo UI (1)

--- app/src/main/java/com/example/to_do_app/activitys/MainActivity.java
+++ app/src/main/java/com/example/to_do_app/activitys/MainActivity.java
@@ -5,6 +5,9 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.to_do_app.R;
@@ -21,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Ánh xạ View
         bottomNav = findViewById(R.id.bottom_nav);

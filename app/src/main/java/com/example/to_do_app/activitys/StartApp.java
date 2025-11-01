1c6c811c19dc817f21015ad7dcb8d2e958806c71
Duong24012006 <duongnguyen.24012006@gmail.com>
2025-11-01 11:22:18 +0700

    [Task-4] cập nhật màn hình bắt đầu

---
+++ app/src/main/java/com/example/to_do_app/activitys/StartApp.java
@@ -0,0 +1,26 @@
+package com.example.to_do_app.activitys;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.to_do_app.R;
+
+public class StartApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_start_app);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
